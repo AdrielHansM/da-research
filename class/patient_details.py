5e25357f2395c5c59c3aecc56b1319aa5f041bd5
AdrielHMG <2191057@slu.edu.ph>
2022-05-16 18:41:51 +0800

    feat: complete user input and output of results using dash

--- class/patient_details.py
+++ class/patient_details.py
@@ -1,228 +1,228 @@
 class Patient:
-  def __init__(self, cluster, age, gender, glucose, cholesterol, systolic_bp, diastolic_bp, mean_arterial_pressure, pregnancies, skin_thickness, insulin, bmi, dpf, age_class, bmi_class, map_class, cholesterol_class, glucose_class, smoke, active, cvd_prediction, diabetes_prediction):
-    self.__cluster                = cluster #KModes cluster of the patient
-    self.__age                    = age #age of the patient
-    self.__gender                 = gender #gender 0 for female and 1 for male
-    self.__glucose                = glucose #glucose level
-    self.__cholesterol            = cholesterol #cholesterol level
-    self.__systolic_bp            = systolic_bp #systolic blood pressure
-    self.__diastolic_bp           = diastolic_bp #diastolic blood pressure
-    self.__mean_arterial_pressure = mean_arterial_pressure #Mean arterial pressure
-    self.__pregnancies            = pregnancies #Number of pregnancies
-    self.__skin_thickness         = skin_thickness #Thickness of skin fold
-    self.__insulin                = insulin #Insulin level
-    self.__bmi                    = bmi #Body mass index
-    self.__dpf                    = dpf #Diabetes pedigree function
-    self.__age_class              = age_class #Age class or Age range
-    self.__bmi_class              = bmi_class #BMI class
-    self.__map_class              = map_class #MAP class
-    self.__cholesterol_class      = cholesterol_class #Cholesterol class
-    self.__glucose_class          = glucose_class #Glucose class
-    self.__smoke                  = smoke #1 if the patient smokes
-    self.__active                 = active #Active or physically active
-    self.__cvd_prediction         = cvd_prediction #Cardiovascular Disease Prediction
-    self.__diabetes_prediction    = diabetes_prediction #Diabetes Prediction
+    def __init__(self, cluster, age, gender, glucose, cholesterol, systolic_bp, diastolic_bp, mean_arterial_pressure, pregnancies, skin_thickness, insulin, bmi, dpf, age_class, bmi_class, map_class, cholesterol_class, glucose_class, smoke, active, cvd_prediction, diabetes_prediction):
+        self.__cluster = cluster    # KModes cluster of the patient
+        self.__age = age    # age of the patient
+        self.__gender = gender    # gender 0 for female and 1 for male
+        self.__glucose = glucose    # glucose level
+        self.__cholesterol = cholesterol    # cholesterol level
+        self.__systolic_bp = systolic_bp    # systolic blood pressure
+        self.__diastolic_bp = diastolic_bp    # diastolic blood pressure
+        self.__mean_arterial_pressure = mean_arterial_pressure    # Mean arterial pressure
+        self.__pregnancies = pregnancies    # Number of pregnancies
+        self.__skin_thickness = skin_thickness    # Thickness of skin fold
+        self.__insulin = insulin    # Insulin level
+        self.__bmi = bmi    # Body mass index
+        self.__dpf = dpf    # Diabetes pedigree function
+        self.__age_class = age_class    # Age class or Age range
+        self.__bmi_class = bmi_class    # BMI class
+        self.__map_class = map_class    # MAP class
+        self.__cholesterol_class = cholesterol_class    # Cholesterol class
+        self.__glucose_class = glucose_class    # Glucose class
+        self.__smoke = smoke    # 1 if the patient smokes
+        self.__active = active    # Active or physically active
+        self.__cvd_prediction = cvd_prediction    # Cardiovascular Disease Prediction
+        self.__diabetes_prediction = diabetes_prediction    # Diabetes Prediction
 
-  def __int__(self):
-      pass
+    def __init__(self):
+        pass
 
-  @property
-  def cluster(self):
-    return self.__cluster
-  
-  @cluster.setter
-  def cluster(self, cluster):
-    self.__cluster = cluster
-  
-  @property
-  def age(self):
-    return self.__age
-  
-  @age.setter
-  def age(self, age):
-    self.__age = age
+    @property
+    def cluster(self):
+        return self.__cluster
 
-  @property
-  def gender(self):
-    return self.__gender
-  
-  @gender.setter
-  def gender(self, gender):
-    self.__gender = gender
+    @cluster.setter
+    def cluster(self, cluster):
+        self.__cluster = cluster
 
-  @property
-  def glucose(self):
-    return self.__glucose
-  
-  @glucose.setter
-  def glucose(self, glucose):
-    self._glucose = glucose
+    @property
+    def age(self):
+        return self.__age
 
-  @property
-  def cholesterol(self):
-    return self.__cholesterol
-  
-  @cholesterol.setter
-  def cholesterol(self, cholesterol):
-    self._cholesterol = cholesterol
+    @age.setter
+    def age(self, age):
+        self.__age = age
 
-  @property
-  def systolic_bp(self):
-    return self.__systolic_bp
-  
-  @systolic_bp.setter
-  def systolic_bp(self, systolic_bp):
-    self._systolic_bp = systolic_bp
+    @property
+    def gender(self):
+        return self.__gender
 
-  @property
-  def diastolic_bp(self):
-    return self.__diastolic_bp
-  
-  @diastolic_bp.setter
-  def diastolic_bp(self, diastolic_bp):
-    self._diastolic_bp = diastolic_bp
+    @gender.setter
+    def gender(self, gender):
+        self.__gender = gender
 
-  @property
-  def mean_arterial_pressure(self):
-    return self.__mean_arterial_pressure
+    @property
+    def glucose(self):
+        return self.__glucose
 
-  @mean_arterial_pressure.setter
-  def mean_arterial_pressure(self, mean_arterial_pressure):
-    self._mean_arterial_pressure = mean_arterial_pressure
+    @glucose.setter
+    def glucose(self, glucose):
+        self.__glucose = glucose
 
-  @property
-  def pregnancies(self):
-    return self.__pregnancies
+    @property
+    def cholesterol(self):
+        return self.__cholesterol
 
-  @pregnancies.setter
-  def pregnancies(self, pregnancies):
-    self.__pregnancies = pregnancies
+    @cholesterol.setter
+    def cholesterol(self, cholesterol):
+        self.__cholesterol = cholesterol
 
-  @property
-  def skin_thickness(self):
-    return self.__skin_thickness
-  
-  @skin_thickness.setter
-  def skin_thickness(self, skin_thickness):
-    self._skin_thickness = skin_thickness
+    @property
+    def systolic_bp(self):
+        return self.__systolic_bp
 
-  @property
-  def insulin(self):
-    return self.__insulin
-  
-  @insulin.setter
-  def insulin(self, insulin):
-    self._insulin = insulin
+    @systolic_bp.setter
+    def systolic_bp(self, systolic_bp):
+        self.__systolic_bp = systolic_bp
 
-  @property
-  def bmi(self):
-    return self.__bmi
-  
-  @bmi.setter
-  def bmi(self, bmi):
-    self._bmi = bmi
+    @property
+    def diastolic_bp(self):
+        return self.__diastolic_bp
 
-  @property
-  def dpf(self):
-    return self.__dpf
-  
-  @dpf.setter
-  def dpf(self, dpf):
-    self._dpf = dpf
+    @diastolic_bp.setter
+    def diastolic_bp(self, diastolic_bp):
+        self.__diastolic_bp = diastolic_bp
 
-  @property
-  def age_class(self):
-    return self.__age_class
-  
-  @age_class.setter
-  def age_class(self, age_class):
-    self._age_class = age_class
+    @property
+    def mean_arterial_pressure(self):
+        return self.__mean_arterial_pressure
 
-  @property
-  def bmi_class(self):
-    return self.__bmi_class
-  
-  @bmi_class.setter
-  def bmi_class(self, bmi_class):
-    self._bmi_class = bmi_class
+    @mean_arterial_pressure.setter
+    def mean_arterial_pressure(self, mean_arterial_pressure):
+        self.__mean_arterial_pressure = mean_arterial_pressure
 
-  @property
-  def map_class(self):
-    return self.__map_class
-  
-  @map_class.setter
-  def map_class(self, map_class):
-    self._map_class = map_class
+    @property
+    def pregnancies(self):
+        return self.__pregnancies
 
-  @property
-  def cholesterol_class(self):
-    return self.__cholesterol_class
-  
-  @cholesterol_class.setter
-  def cholesterol_class(self, cholesterol_class):
-    self._cholesterol_class = cholesterol_class
+    @pregnancies.setter
+    def pregnancies(self, pregnancies):
+        self.__pregnancies = pregnancies
 
-  @property
-  def glucose_class(self):
-    return self.__glucose_class
-  
-  @glucose_class.setter
-  def glucose_class(self, glucose_class):
-    self._glucose_class = glucose_class
+    @property
+    def skin_thickness(self):
+        return self.__skin_thickness
 
-  @property
-  def smoke(self):
-    return self.__smoke
-  
-  @smoke.setter
-  def smoke(self, smoke):
-    self._smoke = smoke
+    @skin_thickness.setter
+    def skin_thickness(self, skin_thickness):
+        self.__skin_thickness = skin_thickness
 
-  @property
-  def active(self):
-    return self.__active
-  
-  @active.setter
-  def active(self, active):
-    self._active = active
+    @property
+    def insulin(self):
+        return self.__insulin
 
-  @property
-  def cvd_prediction(self):
-    return self.__cvd_prediction
-  
-  @cvd_prediction.setter
-  def cvd_prediction(self, cvd_prediction):
-    self._cvd_prediction = cvd_prediction
-  
-  @property
-  def diabetes_prediction(self):
-    return self.__diabetes_prediction
-  
-  @diabetes_prediction.setter
-  def diabetes_prediction(self, diabetes_prediction):
-    self._diabetes_prediction = diabetes_prediction
+    @insulin.setter
+    def insulin(self, insulin):
+        self.__insulin = insulin
 
-  #create a method that returns a dictionary of the values in patient_details
-  def map_patient_details(self):
-    return {
-      "cluster": self.__cluster,
-      "age": self.__age,
-      "gender": self.__gender,
-      "cholesterol": self.__cholesterol,
-      "systolic_bp": self.__systolic_bp,
-      "diastolic_bp": self.__diastolic_bp,
-      "mean_arterial_pressure": self.__mean_arterial_pressure,
-      "skin_thickness": self.__skin_thickness,
-      "insulin": self.__insulin,
-      "bmi": self.__bmi,
-      "dpf": self.__dpf,
-      "age_class": self.__age_class,
-      "bmi_class": self.__bmi_class,
-      "map_class": self.__map_class,
-      "cholesterol_class": self.__cholesterol_class,
-      "glucose_class": self.__glucose_class,
-      "smoke": self.__smoke,
-      "active": self.__active,
-      "cvd_prediction": self.__cvd_prediction,
-      "diabetes_prediction": self.__diabetes_prediction
-    }
+    @property
+    def bmi(self):
+        return self.__bmi
+
+    @bmi.setter
+    def bmi(self, bmi):
+        self.__bmi = bmi
+
+    @property
+    def dpf(self):
+        return self.__dpf
+
+    @dpf.setter
+    def dpf(self, dpf):
+        self.__dpf = dpf
+
+    @property
+    def age_class(self):
+        return self.__age_class
+
+    @age_class.setter
+    def age_class(self, age_class):
+        self.__age_class = age_class
+
+    @property
+    def bmi_class(self):
+        return self.__bmi_class
+
+    @bmi_class.setter
+    def bmi_class(self, bmi_class):
+        self.__bmi_class = bmi_class
+
+    @property
+    def map_class(self):
+        return self.__map_class
+
+    @map_class.setter
+    def map_class(self, map_class):
+        self.__map_class = map_class
+
+    @property
+    def cholesterol_class(self):
+        return self.__cholesterol_class
+
+    @cholesterol_class.setter
+    def cholesterol_class(self, cholesterol_class):
+        self.__cholesterol_class = cholesterol_class
+
+    @property
+    def glucose_class(self):
+        return self.__glucose_class
+
+    @glucose_class.setter
+    def glucose_class(self, glucose_class):
+        self.__glucose_class = glucose_class
+
+    @property
+    def smoke(self):
+        return self.__smoke
+
+    @smoke.setter
+    def smoke(self, smoke):
+        self.__smoke = smoke
+
+    @property
+    def active(self):
+        return self.__active
+
+    @active.setter
+    def active(self, active):
+        self.__active = active
+
+    @property
+    def cvd_prediction(self):
+        return self.__cvd_prediction
+
+    @cvd_prediction.setter
+    def cvd_prediction(self, cvd_prediction):
+        self.__cvd_prediction = cvd_prediction
+
+    @property
+    def diabetes_prediction(self):
+        return self.__diabetes_prediction
+
+    @diabetes_prediction.setter
+    def diabetes_prediction(self, diabetes_prediction):
+        self.__diabetes_prediction = diabetes_prediction
+
+    # create a method that returns a dictionary of the values in patient_details
+    def map_patient_details(self):
+        return {
+            "cluster": str(self.__cluster),
+            "age": str(self.__age),
+            "gender": str(self.__gender),
+            "cholesterol": str(self.__cholesterol),
+            "systolic_bp": str(self.__systolic_bp),
+            "diastolic_bp": str(self.__diastolic_bp),
+            "mean_arterial_pressure": str(self.__mean_arterial_pressure),
+            "skin_thickness": str(self.__skin_thickness),
+            "insulin": str(self.__insulin),
+            "bmi": str(self.__bmi),
+            "dpf": str(self.__dpf),
+            "age_class":str(self.__age_class),
+            "bmi_class": str(self.__bmi_class),
+            "map_class": str(self.__map_class),
+            "cholesterol_class": str(self.__cholesterol_class),
+            "glucose_class": str(self.__glucose_class),
+            "smoke": str(self.__smoke),
+            "active": str(self.__active),
+            "cvd_prediction": str(self.__cvd_prediction),
+            "diabetes_prediction": str(self.__diabetes_prediction)
+        }
--- util/utils.py
+++ util/utils.py
@@ -1,8 +1,8 @@
-def print_string(string):
-    print(string)
-
 def compute_mean_arterial_pressure(systolic, diastolic):
     return ((2* diastolic) + systolic) / 3
+
+def compute_bmi(weight, height):
+    return weight / (height/100)**2
 
 def get_age_class(age):
     if age < 36 :   return 0 #UnderWeight
@@ -10,8 +10,8 @@ def get_age_class(age):
     elif age > 40 and age < 46:  return 2 #OverWeight
     elif age > 45 and age < 51:  return 3 #ClassObesity_1
     elif age > 50 and age < 56:  return 4 #ClassObesity_2
-    elif age > 55 and age < 61:  return 5 #ClassObesity_2
-    elif age > 61:  return 6 #ClassObesity_3
+    elif age > 55 and age < 61:  return 5 #ClassObesity_3
+    elif age > 61:  return 6 #ClassObesity_4
 
 def get_bmi_class(bmi):
     if bmi < 18.5 :   return 0 #UnderWeight
@@ -31,10 +31,39 @@ def get_map_class(mean_arterial_pressure):
 
 def get_cholesterol_class(cholesterol):
     if cholesterol < 201: return 0 #Normal
-    elif cholesterol > 200 and cholesterol < 240:  return 1 #High
+    elif cholesterol > 200 and cholesterol < 240:  return 1 #Medium
     elif cholesterol > 239: return 2 #High
 
 def get_glucose_class(glucose):
     if glucose < 101: return 0 #Normal
-    elif glucose > 100 and glucose < 126:  return 1 #High
+    elif glucose > 100 and glucose < 126:  return 1 #Medium
     elif glucose > 125: return 2 #High
+
+def get_health_status(bmi_class, map_class, cholesterol_class, glucose_class, cvd_prediction, diabetes_prediction):
+    health_status = {}
+
+    health_status['bmi_status'] = ("Underweight" if bmi_class == '0' 
+    else "Normal" if bmi_class == '1'
+    else "Overweight" if bmi_class == '2'
+    else "Obese" if bmi_class == '3'
+    else "Obesity Class 1" if bmi_class == '4'
+    else "Obesity Class 2" if bmi_class == '5' 
+    else "Obesity Class 3")
+
+    health_status['map_status'] = ("Low" if map_class == '0' 
+    else "Normal" if map_class == '1' 
+    else "Elevated" if map_class == '2' 
+    else "Risk of hypertension 1" if map_class == '3' 
+    else "Risk of hypertension 2" if map_class == '4' 
+    else "Requires Emergency Care")
+
+    health_status['cholesterol_status'] = ("Normal" if cholesterol_class == '0' else "Borderline High" if cholesterol_class == '1' 
+    else "High")
+
+    health_status['glucose_status'] = "Normal" if glucose_class == '0' else "Borderline High" if glucose_class == '1' else "High"
+
+    health_status['cvd_risk'] = "Low Risk" if cvd_prediction == '0' else "High Risk"
+
+    health_status['diabetes_risk'] = "Low Risk" if diabetes_prediction == '0' else "High Risk"
+
+    return health_status
